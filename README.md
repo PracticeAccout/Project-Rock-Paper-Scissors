a4e2d523d370f5a511210512a9fd451e537db64a
Yuniel <yreyes8827@gmail.com>
2022-05-05 10:50:39 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Project-Rock-Paper-Scissors
 Requirements
+* Create a function called `computerPlay` that returns randomly either "Rock", "Paper" or "Scissors".
---
+++ rock-paper-scissors.js
@@ -0,0 +1,7 @@
+function computerPlay(){
+    // TODO: Return one of three option randomly:
+    // TODO: Rock, Paper, Scissors
+    // TODO: Part1. Create a func that returns randomly one of three
+    // TODO: numbers: 0, 1, 2
+}
+
