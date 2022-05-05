0cf69752e4023c02e6280cb870cc3c3511e17b44
Yuniel <yreyes8827@gmail.com>
2022-05-05 12:23:09 +0200

    create random function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,7 +1,19 @@
-function computerPlay(){
-    // TODO: Return one of three option randomly:
-    // TODO: Rock, Paper, Scissors
-    // TODO: Part1. Create a func that returns randomly one of three
-    // TODO: numbers: 0, 1, 2
+function randomNum(){
+    // Generates numbers 0-8
+    let rand = (Math.random().toFixed(1) * 10) - 1;
+    if (rand < 0){ // if random is 0-1
+        // console.log(rand);
+        return 0;
+    } else {
+        // console.log(rand);
+        return rand;
+    }
+    
 }
 
+function computerPlay(){
+    let arr = ["Rock", "Paper", "Scissors", 
+                "Rock", "Paper", "Scissors", 
+                "Rock", "Paper", "Scissors"];
+    return arr[randomNum()]
+}
