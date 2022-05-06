358fd88a7d49242ec064dd6bdda4829c9cab5d63
Yuniel <yreyes8827@gmail.com>
2022-05-06 11:55:40 +0200

    add playraound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,4 @@
+// Create random number 
 function randomNum(){
     // Generates numbers 0-8
     let rand = (Math.random().toFixed(1) * 10) - 1;
@@ -10,10 +11,35 @@ function randomNum(){
     }
     
 }
-
+// Computer choice
 function computerPlay(){
     let arr = ["Rock", "Paper", "Scissors", 
                 "Rock", "Paper", "Scissors", 
                 "Rock", "Paper", "Scissors"];
     return arr[randomNum()]
 }
+
+// Create one round competition
+function playRound(playerSelection, computerSelection){
+    // Paper > Rock > Scissor > Paper
+    // Paper - Rock   || Paper - Scissors - || Paper-Paper
+    // Rock- Scissors || Rock - Rock 
+    // Scissors - Scissors
+    // console.log(playerSelection);
+    // console.log(computerSelection);
+
+    if (computerSelection.toLowerCase() === playerSelection.toLowerCase()){
+        return "We're tied"
+    } 
+    if (computerSelection.toLowerCase() === "paper" && playerSelection.toLowerCase() === "rock"){
+        return "You Lose! Paper beats Rock";
+    }  
+    if (computerSelection.toLowerCase() === "rock"  && playerSelection.toLowerCase() === "scissors"){
+        return "You Lose! Rock beats Scissors";
+    }
+    if (computerSelection.toLowerCase() === "scissors" && playerSelection.toLowerCase() === "paper") {
+        return "You Lose! Scissors beats Paper";
+    }
+    return `You won! ${playerSelection} beats ${computerSelection}.`;
+}
+
